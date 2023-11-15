ac30a8bb0ab85144ede9eca7f8f481a0da857665
Volodya <chuchman002@gmail.com>
2023-11-15 18:12:54 +0200

    Add project files.

---
+++ Lab №8.2/Lab №8.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+size_t CountLastWord(const std::string s) {
+    int count = 0;
+    size_t start = 0, finish = 0;
+    bool foundLastWord = false;
+
+    while ((start = s.find_first_not_of(" ", finish)) != std::string::npos) {
+        finish = s.find_first_of(" ", start);
+        if (finish == std::string::npos) {
+            finish = s.length();
+            foundLastWord = true;
+        }
+        if (foundLastWord) {
+            for (size_t i = start; i < finish; ++i) {
+                if (s[i] == 'a' || s[i] == 'A') {
+                    count++;
+                }
+            }
+        }
+        start = finish + 1;
+    }
+    return count;
+}
+
+int main() {
+    std::string str;
+    std::cout << "Print letter string:" << std::endl;
+    std::getline(std::cin, str);
+    std::cout << "number of a: " << CountLastWord(str) << std::endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include "../Lab №8.2/Lab №8.2.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestCountAsInLastWord)
+		{
+			
+			std::string testString = "I eat apples";
+			size_t expectedCount = 1; 
+
+			
+			size_t actualCount = CountLastWord(testString);
+
+			
+			Assert::AreEqual(expectedCount, actualCount);
+		}
+	};
+}
